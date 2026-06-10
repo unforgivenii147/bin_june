1ef9ccce3e95223e953e3f36fd73c57b9ce3953a
unforgivenii147 <adnanonagh@gmail.com>
2026-06-10 23:32:36 +0430

    2026-06-10 23:32:35

--- git_back_to_main.sh
+++
@@ -1,4 +0,0 @@
-git switch -c temp-branch    # Save current work to a new branch
-git switch main              # Switch to main
-git merge temp-branch        # Merge your work into main
-git branch -d temp-branch    # Delete temp branch
